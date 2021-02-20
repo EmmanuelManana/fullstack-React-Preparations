e5e874359f6902a97f187a00baee6fbfd399a850
EmmanuelManana <emanana@student.wethinkcode.co.za>
2021-02-20 10:01:58 +0200

    user Auth Rediction handled

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,40 @@
-import express from "express";
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+const keys = require("./config/keys");
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
+// console.developers.google.com, google+ -> OAuth
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 
-//Middleware
-app.use(express.json())
+// MiddleWare
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.status(200).send("<h1>creating a server</h1>")
+//  routes
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  }),
+  (req, res) => {}
+);
+
+app.get('/auth/google/callback', (req, res) => {
+    res.status(200).send({ redirected : "user has been redirected, back to our side"})
 })
 
-app.listen(PORT, () =>{
-    console.log(`server running on http://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`server running on http://localhost:${PORT}`);
+});
