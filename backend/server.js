5c1bb040ad06a7ef3384e70d3f324d389f71a364
EmmanuelManana <emanana@student.wethinkcode.co.za>
2021-02-21 08:26:32 +0200

    a quick formart

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,13 @@ require("./models/User");
 require("./services/passport"); //uses googleOauth
 require("dotenv/config");
 
-// database
-const mongoose = require("mongoose");
-
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// database
+const mongoose = require("mongoose");
 const DB_CONNECTION = process.env.DB_CONNECT;
+
 // MiddleWare
 app.use(express.json());
 app.use("/auth", authRoutes);
@@ -18,20 +19,17 @@ app.get("/", (req, res) => {
   res.status(200).send({ object: "response object" });
 });
 
-mongoose
-  .connect(
-    DB_CONNECTION,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
-  .then(() => {
-    console.log("connected to DB");
-  })
-  .catch(error => {
-    console.error("db connection error => ", error);
-  });
+mongoose.connect(
+  DB_CONNECTION,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+).then(() => {
+  console.log("connected to DB");
+}).catch(error => {
+  console.error("db connection error => ", error);
+});
 
 app.listen(PORT, () => {
   console.log(`server running on http://localhost:${PORT}`);
