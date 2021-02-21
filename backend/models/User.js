85ec9796fccfa5b7417c43673d93c43985811a8d
EmmanuelManana <emanana@student.wethinkcode.co.za>
2021-02-21 08:22:18 +0200

    uding destructured schema from mongoose

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,19 +1,19 @@
 const mongoose = require("mongoose");
 // destructure schema from mongoose
-// const {Schema} = mongoose;
+const {Schema} = mongoose;
 
-const userSchema = mongoose.Schema({
-    googleId : {
-        type: String,
-        required: true
-    }
-})
-
-// const userSchema = new Schema({
+// const userSchema = mongoose.Schema({
 //     googleId : {
 //         type: String,
 //         required: true
 //     }
 // })
+
+const userSchema = new Schema({
+    googleId : {
+        type: String,
+        required: true
+    }
+})
 
 module.exports = mongoose.model('users', userSchema)
