805721c50ba82f4288949f68a9d254d2638b6407
EmmanuelManana <emanana@student.wethinkcode.co.za>
2021-02-24 23:53:01 +0200

    need proper commit sys

--- frontend/src/reducers/authReducer.js
+++ frontend/src/reducers/authReducer.js
@@ -1,6 +1,9 @@
 const authReducer = (state = [], action) => {
   console.log("check payload: => ", action.payload)
   switch (action.type) {
+    case "FETCH_USER":
+      // update state:
+      break;
     default:
       return state;
   }
